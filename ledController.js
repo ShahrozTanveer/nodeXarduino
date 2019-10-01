e69502145c193f8c2dfba679b7b2ed378f8af229
Sharoz Tanveer <saadtanveer7@live.co.uk>
2019-10-02 00:08:22 +0500

    ledController

---
+++ ledController.js
@@ -0,0 +1,24 @@
+"use strict";
+var five = require('johnny-five'),
+    express = require('express'),
+    expressLayouts = require('express-ejs-layouts'),
+    app = express(),
+    board = new five.Board(),
+    led = null,
+    port = process.env.PORT || 8000,
+    flag = false;
+
+board.on('ready', function() {
+    console.log('board ready');
+    led = new five.Led(13); //pin 13 on board
+    exports.LED = led;
+    flag = true;
+    app.use(expressLayouts);
+    app.set('view engine', 'ejs');
+    app.use('/', require('./routes/index'));
+    app.use('/components', require('./routes/components'));
+    app.listen(port, console.log(`APP IS RUNNING ON PORT: ${port}`));
+});
+if (!flag) {
+    console.log("waiting for device to connect....");
+}
---
+++ routes/components.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const led = require('../ledController').LED;
+router.get('/led', (req, res) => res.render('led'));
+router.get('/led/:in', (req, res) => {
+    console.log(req.params.in);
+    let input = req.params.in;
+    if (input == 'on') {
+        led.on();
+        console.log("led on");
+    } else if (input == 'blink') {
+        led.blink();
+        console.log("led blink");
+    } else if (input == 'off') {
+        led.off();
+        console.log("led off");
+    } else if (input == 'stop') {
+        led.stop();
+        console.log("led stop");
+    } else {
+        console.log("unknown input");
+        //alert("unknown input");
+    }
+    res.render('led')
+});
+module.exports = router;
